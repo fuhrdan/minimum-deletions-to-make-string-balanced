2f1e04a2f4f1f45725b256aafa8810372c708aad
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-07-30 09:10:21 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//*****************************************************************************
+//** 1653. Minimum Deletions to Make String Balanced  leetcode               **
+//** Solution in C.  Started with a left and right, then moved into one-side **
+//**                                                                -Dan     **
+//*****************************************************************************
+
+
+int minimumDeletions(char* s) {
+    int n = strlen(s);
+    int countB = 0; 
+    int minDeletions = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (s[i] == 'b') {
+            ++countB;
+        } else {
+            if (countB > 0) {
+                ++minDeletions;
+                --countB;
+            }
+        }
+    }
+
+    return minDeletions;
+}
